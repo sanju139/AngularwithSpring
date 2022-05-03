42c79c2e52d000c21d08ea162226aa058022a3e3
sanju139 <60957202+sanju139@users.noreply.github.com>
2022-05-03 12:56:15 +0530

    Add files via upload

---
+++ src/test/java/com/demo/firstapp/Myapp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myapp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
